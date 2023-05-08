540b9ce270db7e02609a70fc189d8831bd34bb94
jwzhou1} <jwzhou1@github.com>
2023-05-08 16:18:50 +0800

    finish create_triangle

--- cpractice.h
+++ cpractice.h
@@ -270,7 +270,11 @@ Polygon* create_rectangle(int width, int height){
  * width, height
 */
 Polygon* create_triangle(int width, int height){
-    return NULL;
+    Polygon *triangle = create_polygon(3);
+    triangle->points[0] = create_point(0, 0);
+    triangle->points[1] = create_point(width, 0);
+    triangle->points[2] = create_point(width, height);
+    return triangle;
 }
 
 /**
